5780ba803ec0250909bc53a19c019bec199e03e2
Anuja Prakash Kolse <anujaprakashkolse@Anujas-MBP.cable.rcn.com>
2023-06-19 15:07:30 -0400

    modified session code in app js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import UserController from './users/users-controller.js';
 import TuitsController from './controllers/tuits/tuits-controller.js';
 import AuthController from "./users/auth-controller.js";
 
-const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/tuiter' //'mongodb+srv://Cluster57626:anjali@cluster0.jyy3myf.mongodb.net/tuiter'
+const CONNECTION_STRING =  'mongodb+srv://Cluster57626:anjali@cluster0.jyy3myf.mongodb.net/tuiter' //'mongodb://127.0.0.1:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
 
 const app = express()
@@ -15,14 +15,18 @@ app.use(
     session({
       secret: "any string",
       resave: false,
-      saveUninitialized: true
+      saveUninitialized: false,
+      cookie: {
+        sameSite: "none",
+        secure: true,
+      },
     })
 );
 
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:3000",//"https://a5--monumental-starburst-c22729.netlify.app",
+    origin: "https://a5--monumental-starburst-c22729.netlify.app", //"http://localhost:3000",//
   })
 )
 
